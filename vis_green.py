64b579fd8302b410e8d050f1a2d1e7d1975ce9ef
yoneshin357 <yoneshin357@gmail.com>
2025-01-26 10:43:56 +0900

    Update vis_green.py

--- vis_green.py
+++ vis_green.py
@@ -34,9 +34,9 @@ path = 'https://github.com/yoneshin357/g_finder/raw/refs/heads/main/'
 # CSVデータを読み込む
 
 #kilo = pd.read_excel("C:/Users/yone/Documents/basic_DB/mars_kilo/地図情報基盤システムキロ標データ.xlsx")
-kilo = pd.read_csv(path+"地図情報基盤システムキロ標データ.csv")
+#kilo = pd.read_csv(path+"地図情報基盤システムキロ標データ.csv")
 #data = pd.read_excel(path+"karasuyama.xlsx")
-data = pd.read_csv(path+"karasuyama.csv")
+#data = pd.read_csv(path+"karasuyama.csv")
 #,encoding='cp932
 print("行、列=",data.shape)
 
