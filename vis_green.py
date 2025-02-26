503be6c4e1e07f0b1760b28067335728c647f041
yoneshin357 <yoneshin357@gmail.com>
2025-02-26 14:58:37 +0900

    Update vis_green.py

--- vis_green.py
+++ vis_green.py
@@ -306,8 +306,9 @@ with tab1:
                 longitude=140.02,
                 zoom=7,
                 pitch=50,
+                use_container_width=True,
                 width="100%",  # 幅を800ピクセルに設定
-                height="100%"  # 高さを600ピクセルに設定
+                height=1200  # 高さを600ピクセルに設定
             ),
             layers=[
 
